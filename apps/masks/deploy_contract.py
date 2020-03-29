934188b9912d690126c3caf50b85ffebf343a679
Sylvain Bellemare <sbellem@gmail.com>
2020-03-28 22:52:22 -0500

    wip: separation of key components

---
+++ apps/masks/deploy_contract.py
@@ -0,0 +1,48 @@
+from pathlib import Path
+
+from web3 import HTTPProvider, Web3
+
+from apps.utils import create_and_deploy_contract
+
+PARENT_DIR = Path(__file__).resolve().parent
+PUBLIC_DATA_DIR = "public-data"
+CONTRACT_ADDRESS_FILENAME = "contract_address"
+CONTRACT_ADDRESS_FILEPATH = PARENT_DIR.joinpath(
+    PUBLIC_DATA_DIR, CONTRACT_ADDRESS_FILENAME
+)
+
+
+def deploy(
+    *, contract_name, contract_filepath, n=4, t=1, eth_rpc_hostname, eth_rpc_port
+):
+    w3_endpoint_uri = f"http://{eth_rpc_hostname}:{eth_rpc_port}"
+    w3 = Web3(HTTPProvider(w3_endpoint_uri))
+    deployer = w3.eth.accounts[49]
+    mpc_addrs = w3.eth.accounts[:n]
+    contract_address, abi = create_and_deploy_contract(
+        w3,
+        deployer=deployer,
+        contract_name=contract_name,
+        contract_filepath=contract_filepath,
+        args=(mpc_addrs, t),
+    )
+    return contract_address
+
+
+if __name__ == "__main__":
+    contract_name = "MpcCoordinator"
+    contract_filename = "contract.sol"
+    contract_filepath = PARENT_DIR.joinpath(contract_filename)
+    eth_rpc_hostname = "blockchain"
+    eth_rpc_port = 8545
+    n, t = 4, 1
+    contract_address = deploy(
+        contract_name=contract_name,
+        contract_filepath=contract_filepath,
+        t=1,
+        eth_rpc_hostname=eth_rpc_hostname,
+        eth_rpc_port=eth_rpc_port,
+    )
+
+    with open(CONTRACT_ADDRESS_FILEPATH, "w") as f:
+        f.write(contract_address)
