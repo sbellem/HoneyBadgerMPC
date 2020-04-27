04c5983616bc827868d71e5187a7d351d5fea1a0
Sylvain Bellemare <sbellem@gmail.com>
2020-04-27 00:31:49 -0500

    wip: docker-compose to run MPC servers separately

---
+++ apps/masks/follow-sim-logs-with-tmux.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+if [ -z $TMUX ]; then
+    echo "tmux is not active, will start new session"
+    TMUX_CMD="new-session"
+else
+    echo "tmux is active, will launch into new window"
+    TMUX_CMD="new-window"
+fi
+
+tmux $TMUX_CMD "docker-compose -f sim.yml logs -f blockchain; sh" \; \
+    splitw -h -p 50 "docker-compose -f sim.yml logs -f hbmpc.coordinator.io; sh" \; \
+    splitw -v -p 50 "docker-compose -f sim.yml logs -f hbmpc.peer0.io; sh" \; \
+    splitw -h -p 50 "docker-compose -f sim.yml logs -f hbmpc.peer1.io; sh" \; \
+    splitw -h -p 50 "docker-compose -f sim.yml logs -f hbmpc.peer2.io; sh" \; \
+    splitw -h -p 50 "docker-compose -f sim.yml logs -f hbmpc.peer3.io; sh" \; \
+    selectp -t 0 \; \
+    splitw -v -p 50 "docker-compose -f sim.yml logs -f hbmpc.client.io; sh"
